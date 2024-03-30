56e89b29a7139fa12116be451ce447affff0a5f1
Aditya-Narayan-Maurya <147300569+Aditya-Narayan-Maurya@users.noreply.github.com>
2024-03-30 14:32:30 +0530

    handling deletion using mongoose middleware and pre and post

--- models/customer.js
+++ models/customer.js
@@ -19,24 +19,79 @@ const customerSchema= new Schema({
     order:[{ type: Schema.Types.ObjectId, ref: 'Order'}]
 });
 
+// customerSchema.pre('findOneAndDelete', async()=> {
+//   console.log("This is pre")
+// });
+
+customerSchema.post('findOneAndDelete', async(customer)=>{
+  if (customer.order.length) {
+    let data=await Order.deleteMany({_id:{$in:customer.order}})
+    console.log(data);
+  }
+});
 const Order = mongoose.model('Order', orderSchema);
 const Customer = mongoose.model('Customer', customerSchema);
 
-const addCustomer=async()=>{
-//     let cust1=new Customer({
-//         name:"rahul kumar"
-//     })
-//   let order1= await Order.findOne({item:"chips"});
-//   let order2= await Order.findOne({item:"chocolate"});
-//   cust1.order.push(order1);
-//   cust1.order.push(order2);
-//   let result= await cust1.save();
-//   console.log(result);
-  let result= await Customer.find();
-  console.log(result);
+//functions
+const findCustomer=async()=>{
+    let result = await Customer.find({}).populate("orders");
+    console.log(result[0]);
+};
 
+
+
+//add customer and orders
+const addCust=async()=>{
+   let newCust= new Customer({
+    name:"karan arjun",
+   });
+  const newOrder= new Order({
+    item:"burger",
+    price:250
+  });
+  newCust.order.push(newOrder);
+  await newCust.save();
+  await newOrder.save();
+  console.log("added new customer");
 }
-addCustomer();
+// addCust();
+
+//delete customer and order
+const delCust=async()=>{
+  let data= await Customer.findByIdAndDelete('6607ce874bd7f8ef8f1accf3');
+  console.log(data);
+  console.log("data has been delete");
+}
+delCust();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const addCustomer=async()=>{
+// //     let cust1=new Customer({
+// //         name:"rahul kumar"
+// //     })
+// //   let order1= await Order.findOne({item:"chips"});
+// //   let order2= await Order.findOne({item:"chocolate"});
+// //   cust1.order.push(order1);
+// //   cust1.order.push(order2);
+// //   let result= await cust1.save();
+// //   console.log(result);
+//   let result= await Customer.find();
+//   console.log(result);
+
+// }
+// addCustomer();
 
 
 
